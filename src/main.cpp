55508bcd5f1cabca8c7aa86a3d9a52935b617079
Gabriel <gabi.rytov@gmail.com>
2021-08-21 23:45:32 -0400

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+// #include <lvgl.h>
+#include <TFT_HX8357.h> // Hardware-specific library
+
+TFT_HX8357 tft = TFT_HX8357();       // Invoke custom library
+
+void setup(){
+
+}
+
+void loop(){
+  
+}
