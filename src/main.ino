57b06bc36690f0bed61c0c13efe6170f59a6d490
Gabriel Rytov <gabi.rytov@gmail.com>
2021-09-20 18:05:28 +0300

    Pump scheduler and fill() completed

--- src/main.ino
+++ src/main.ino
@@ -2,6 +2,7 @@
 #include <MCUFRIEND_kbv.h>
 #include <TouchScreen.h>
 #include <Adafruit_GFX.h>
+#include <Scheduler.h>
 #include <SD.h>
 #include <SPI.h>
 #include <Pump.h>
@@ -9,30 +10,30 @@
 #include <string.h>
 using namespace std;
 
-const uint8_t chipSelect = 10; // Sets the SD chip-select pin, enabled on Mega through SOFT-SPI
-char *drinkName;
-
 parser Parser;
 Pump pump1(1), pump2(2), pump3(3), pump4(4), pump5(5), pump6(6), pump7(7), pump8(8), pump9(9), pump10(10);
 Pump *allPumps[10] = {&pump1, &pump2, &pump3, &pump4, &pump5, &pump6, &pump7, &pump8, &pump9, &pump10};
 
-// void pump(int *mL, char *liquor[30])
-// {
-//     // unsigned long currentMillis;
-//     // unsigned long lastMillis;
-//     // unsigned int onMillis;
-//     int seconds = *mL * 0.6;
-//     for (unsigned short i = 0; i < sizeof(allPumps); i++)
-//     {
-//         if (strcmp(allPumps[i]->drink, *liquor) == 0)
-//         {
-//             // currentMillis = millis();
-//             allPumps[i]->on();
-//             delay(seconds * 1000);
-//             allPumps[i]->off();
-//         }
-//     }
-// }
+unsigned int ingrLength = sizeof(Parser.cocktail.ingridients) / sizeof(Parser.cocktail.ingridients[0]);
+unsigned int pumpLength = sizeof(allPumps) / sizeof(allPumps[0]);
+const uint8_t chipSelect = 10; // Sets the SD chip-select pin, enabled on Mega through SOFT-SPI
+
+void fill(char drinkName[30])
+{
+    Parser.getRecipe(drinkName); //Saves recipe to memory
+    for (int i = 0; i < ingrLength; i++)
+    {
+        for (int n = 0; n < pumpLength; n++)
+        {
+            if (strcmp(allPumps[n]->drink, Parser.cocktail.ingridients[i].liquor) == 0)
+            {
+                allPumps[n]->on(atoi(Parser.cocktail.ingridients[i].portion[0]));
+                Serial.println(allPumps[n]->drink);
+                break;
+            }
+        }
+    }
+}
 
 void setup()
 {
@@ -47,31 +48,48 @@ void setup()
     }
     Serial.println("SD initialization done.");
 
-    pump1.drink = "campari";
-    pump2.drink = "vodka";
-    pump3.drink = "soda water";
-    pump4.drink = "gin";
-    pump5.drink = "lemon juice";
-    pump6.drink = "cointreau";
-    pump7.drink = "red vermouth";
-    pump8.drink = "triple sec";
-    pump9.drink = "simple syrup";
-    pump10.drink = "white vermouth";
+    pump1.label("simple syrup");
+    pump2.label("cointreau");
+    pump3.label("soda water");
+    pump4.label("gin");
+    pump5.label("lemon juice");
+    pump6.label("cointreau");
+    pump7.label("lime juice");
+    pump8.label("passionfruit syrup");
+    pump9.label("white rum");
+    pump10.label("grenadine");
 
     Parser.setFile("RECIPES.txt");
     Parser.getOptions(allPumps);
-    for (uint8_t i = 0; i < sizeof(Parser.drinksBuffer[0]); i++)
+
+    uint8_t i = 0;
+    while (Parser.drinksBuffer[0][i] != '\0')
     {
-        if (Parser.drinksBuffer[0][i] == '\0')
-        {
-            break;
-        }
-        else
-            Serial.println(Parser.drinksBuffer[i]);
+        Serial.println(Parser.drinksBuffer[i]);
+        i++;
     }
+
+    fill("Daiquiri");
+    Scheduler.startLoop(pumpScheduler);
 }
 
 void loop()
 {
-  
+}
+
+void pumpScheduler()
+{
+    unsigned long currentMillis = millis();
+    unsigned long millisDifference;
+    for (int n = 0; n < pumpLength; n++)
+    {
+        if (allPumps[n]->status == true)
+        {
+            millisDifference = currentMillis - allPumps[n]->lastMillis;
+            if (millisDifference >= allPumps[n]->delayTime)
+            {
+                allPumps[n]->off();
+            }
+        }
+    }
 }
